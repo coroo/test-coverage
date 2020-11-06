e581ba5a3daa31bf294c9a993a827b04b9921fba
coroo <coroo.wicaksono@gmail.com>
2020-11-06 22:05:51 +0700

    :pencil2: replace some text

--- dist/index.js
+++ dist/index.js
@@ -2254,15 +2254,11 @@ function createMessage(pytestResult) {
     newMessage += "Test";
     let lastMessage = "";
     for (let i in lineOfText) {
-        if (lineOfText[i].indexOf('coverage: platform darwin,') >= 0) {
+        if (lineOfText[i].indexOf('coverage: platform') >= 0) {
             startKey = i;
             newMessage += "\n" + lineOfText[i] + "\n";
             delete lineOfText[i];
             console.log(newMessage);
-        }
-        else {
-            newMessage += "Tidak Masuk Key";
-            newMessage += lineOfText[i];
         }
         if (startKey != "0" && lineOfText[i] != undefined) {
             if (lineOfText[i].indexOf('Name                                  Stmts   Miss  Cover') >= 0) {
